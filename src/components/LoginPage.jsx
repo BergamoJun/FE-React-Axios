dc7e89bfc6e0bd735479b66513208e420a2225e5
june <wnsgmlb501@naver.com>
2025-06-03 00:25:35 +0900

    done

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import "../styles/LoginPage.css";
+  import React, { useState } from "react";
+  import axios from "axios";
+  import { useNavigate } from "react-router-dom";
+  import "../styles/LoginPage.css";
 
-function LoginForm() {
-  
+  function LoginForm() {
+    function setEmail(val){
+      localStorage.setItem("email",val);
+    }
+    function setPassword(val){
+      localStorage.setItem("password",val);
+    }
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    const handleLogin = async (e) => {
+      e.preventDefault();
+      try{
+        const data = await axios.post(
+          "https://reqres.in/api/login",{
+          email: email,
+          password: pwd
+        },{
+          headers: {
+            "x-api-key": "reqres-free-v1"
+          }
+        }
+      )
+      }catch(e){
+        console.log(e);
+      }
+    };
+    return (
+      <form onSubmit={handleLogin} className="login-container">
+        <h2>🔐 로그인</h2>
+        <input type="email" placeholder="이메일" required onChange={(e)=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="비밀번호" required onChange={(e)=>setPassword(e.target.value)}/>
+        <button type="submit">로그인</button>
+      </form>
+    );
+  }
 
-
-    
-  };
-
-  return (
-    <form onSubmit={handleLogin} className="login-container">
-      <h2>🔐 로그인</h2>
-      <input/>
-      <input/>
-      <button type="submit">로그인</button>
-    </form>
-  );
-}
-
-export default LoginForm;
+  export default LoginForm;
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,22 +3,55 @@ import axios from "axios";
 import "../styles/UserList.css";
 
 function UserList() {
+  const [users,setUsers] = useState([]);
+  const [keyword,setKeyword] = useState("");
 
+  useEffect(()=>{
+    async ()=>{
+      try{
+        const ag = await axios.get(
+          "https://reqres.in/api/users?page=2",
+          {
+            headers:{
+              "x-api-key": "reqres-free-v1"
+            }
+          }  
+        );
+        setUsers(ag.data.data);
+      }catch(e){
+        console.log(e);
+      }
+    }
+  },[]);  
+
+  const search = users.filter((u)=>{
+    const info = `${u.first_name}${u.last_name}<br>${u.email}`;
+    return info.includes(keyword);
+  })
   return (
     <div className="user-container">
       <div className="user-info-box">
-        <p><strong>이메일:</strong> { }</p>
-        <p><strong>토큰:</strong> { }</p>
+        <p><strong>이메일:</strong>{localStorage.getItem('email')}</p>
+        <p><strong>토큰:</strong> {localStorage.getItem('token')}</p>
       </div>
 
       <h2 className="user-title">👥 유저 목록</h2>
 
       <input
         className="user-search"
+        placeholder="이름 또는 이메일로 검색하세요"
+        value={keyword}
+        onChange={(e)=>{setKeyword(e.target.value)}}
       />
 
       <div className="user-list">
-     
+        {search.length?search.map((u)=>(
+          <div className="user-card">
+          <img className="user-avatar" src={u.avatar} alt="img"/>
+          <p>{u.first_name} {u.last_name}</p> 
+          <p className="user-email">{u.email}</p> 
+          </div>
+        )):<div>검색 결과가 없습니다.</div>}        
       </div>
     </div>
   );
